a601c7a778a8ddf18a1f461efe31a899993b3a9b
HyoSean <kimhyosun@gmail.com>
2022-01-25 16:32:04 +0900

    검색 이름 리스트 완료 및 이미지 리스트 API 데이터 가공 중간저장

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,29 +3,39 @@ import {useEffect, useState} from 'react';
 
 const Search = () => {
 	const [list, setList] = useState([]);
-	//const [searchText, setSearchText] = useState('');
-	//const [dog, setDog] = useState();
-	useEffect(() => {
-		axios.get('https://dog.ceo/api/breeds/list/all').then((res) => setList(res.data.message));
-	}, []);
+	const [dog, setDog] = useState([]);
+	const [imgUrl, setImgurl] = useState();
+	const getData = async () => {
+		const response = await axios.get('https://dog.ceo/api/breeds/list/all');
+		const list = Object.keys(response.data.message);
+		setList(list);
+	};
+	useEffect(() => getData(), []);
 
 	const onSearch = (e) => {
 		let text = e.target.value;
-		console.log(list);
-		const kkk = [];
-		list.keys(kkk);
-		const {affenpinscher} = list;
-		console.log(kkk);
-		// console.log(it);
-		//setSearchText();
 
-		//axios.get(`https://dog.ceo/api/breed/${searchText}/images`).then((res) => console.log(res.data.message[0]));
+		const searchList = list.filter((cur) => !cur.indexOf(text));
+		console.log(searchList);
+		setDog(searchList);
+
+		const imgs = searchList.map((cur) => axios.get(`https://dog.ceo/api/breed/${cur}/images`));
+		console.log(imgs.message[0]);
+		setImgurl(imgs.message[0]);
 	};
+
 	return (
 		<>
 			<strong>SEARCH</strong>
 			<input type="text" className="search_bar" onInput={(e) => onSearch(e)} />
-			{/* <img src={dog} alt={searchText} /> */}
+			<ul>
+				{dog.map((cur, idx) => (
+					<li key={idx}>
+						{cur}
+						<img src={imgUrl} alt={cur} />
+					</li>
+				))}
+			</ul>
 		</>
 	);
 };
